61a59cbe967d0d86e01daadb1ee04517014532c6
Lydoww <alexis94340@gmail.com>
2025-03-01 18:19:42 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Grid, GridItem, Stack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Stack hideBelow={"lg"}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Stack>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
