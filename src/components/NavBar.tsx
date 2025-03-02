3b933b773e2ea512f8f05e2fe04e66a97dfce310
Lydoww <alexis94340@gmail.com>
2025-03-02 19:33:05 +0100

    dark/light mode added in NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
+import Button from "./Button";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px"></Image>
       <Text>NavBar</Text>
+      <Button />
     </HStack>
   );
 };
