02ef77ff72cd06706a71c9f158d64c4bcd306119
Lydoww <alexis94340@gmail.com>
2025-03-04 00:02:46 +0100

    create logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,21 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.svg";
+import { HStack, Image, Link, Text } from "@chakra-ui/react";
+import logo from "../assets/rawg-like.svg";
 import Button from "./Button";
 
 const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"} padding={"10px"}>
-      <Image src={logo} boxSize="60px"></Image>
+      <Link href="/">
+        <Image
+          src={logo}
+          boxSize="60px"
+          _hover={{ scale: 1.1, transition: "0.3s ease" }}
+        ></Image>
+      </Link>
+
+      <Text fontSize={"2xl"} fontWeight={"bold"}>
+        RawgLike
+      </Text>
       <Button />
     </HStack>
   );
