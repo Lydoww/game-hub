a9b185a42fb8dc9a9150bdee03154c3675436eba
Lydoww <alexis94340@gmail.com>
2025-03-03 19:49:03 +0100

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,7 +14,7 @@ const GameCard = ({ game }: Props) => {
   }
 
   return (
-    <Card.Root maxW="sm" overflow="hidden">
+    <Card.Root width={"300px"} borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageURL(game.background_image)} alt={game.name} />
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
